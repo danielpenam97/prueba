cc44e2eaaa438dd9ade8f2705799a5d9b81e65e0
unknown <danielpenam97@gmail.com>
2019-03-05 20:53:42 -0500

    Initial commit

---
+++ public/prueba.py
@@ -0,0 +1,17 @@
+num1 = int(input('Ingrese un numero: '))
+num2 = int(input('Ingrese un segundo numero: '))
+
+
+if num1 == num2:
+    print('son iguales')
+elif num1 != num2:
+    print('son diferentes')
+else:
+    print('Ha ocurrido un error')
+i = 1
+for i in range (5):
+    print(i)
+
+while num1 == num2:
+    print ('son iguales')
+    break
